43011b771cfc4b135abf01ece601c2d8fb12fb98
David <xodnjs2549@gmail.com>
2019-09-14 02:03:40 +0900

    Add remove main block function

--- src/main/resources/static/article.js
+++ src/main/resources/static/article.js
@@ -4,6 +4,7 @@ window.onload = function () {
     addOnClickToMainBlockBtns(addMainBlockBtns, addMainBlock);
 
     var removeMainBlockBtns = document.querySelectorAll("[name=remove-main-block]");
+    console.log(removeMainBlockBtns);
     addOnClickToMainBlockBtns(removeMainBlockBtns, removeMainBlock);
 };
 
@@ -11,6 +12,7 @@ function addMainBlock(event) {
     var templateHtml = document.getElementById("main-block-template").innerHTML;
     // 클릭된 플러스 버튼이 속한 메인블록 아래에 메인블록이 추가된다
     var clickedMainBlock = event.target.closest(".main-block");
+    console.log(clickedMainBlock);
     clickedMainBlock.insertAdjacentHTML('afterend', templateHtml);
 
     // 인터벌 걸지 않고 그냥 show 클래스를 추가하면 css의 transition이 먹지 않는다 왜..
@@ -18,15 +20,18 @@ function addMainBlock(event) {
         createdMainBlock.classList.add("show");
     }, 1)
 
-    // 새롭게 생성된 메인블록
+    // 새롭게 생성된 메인블록의 메인블록 생성/제거 버튼에 onClick 메소드 붙이기
     var createdMainBlock = clickedMainBlock.nextElementSibling;
-    var createdAddMainBlockBtn = createdMainBlock.querySelector("[name=add-main-block]");
-    createdAddMainBlockBtn.onclick = addMainBlock;
+    createdMainBlock.querySelector("[name=add-main-block]").onclick = addMainBlock;
+    createdMainBlock.querySelector("[name=remove-main-block]").onclick = removeMainBlock;
 }
 
 function removeMainBlock(event) {
     var clickedMainBlock = event.target.closest(".main-block");
-    clickedMainBlock.remove();
+        clickedMainBlock.classList.remove("show");
+    setInterval(function () {
+        clickedMainBlock.remove();
+    }, 300)
 }
 
 function addOnClickToMainBlockBtns(mainBlockBtns, onClickMethod) {
