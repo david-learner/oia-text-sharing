4ad0baecafc39e9903d800cdf31894b0464d065d
David <xodnjs2549@gmail.com>
2019-09-14 01:48:19 +0900

    Fix addOnClickToMainBlockBtns

--- src/main/resources/static/article.js
+++ src/main/resources/static/article.js
@@ -1,11 +1,12 @@
 window.onload = function () {
-    // 페이지 로딩이 끝나면 메인블록 플러스 버튼에 온클릭 메소드를 붙인다
+    // 페이지 로딩이 끝나면 메인블록 플러스 및 마이너스 버튼에 온클릭 메소드를 붙인다
     var addMainBlockBtns = document.querySelectorAll("[name=add-main-block]");
-    console.log(addMainBlockBtns.length);
-    addOnClickToAddMainBlockBtns(addMainBlockBtns);
+    addOnClickToMainBlockBtns(addMainBlockBtns, addMainBlock);
+
+    var removeMainBlockBtns = document.querySelectorAll("[name=remove-main-block]");
+    addOnClickToMainBlockBtns(removeMainBlockBtns, removeMainBlock);
 };
 
-// +로 추가된 애들의 +버튼에도 메인블럭을 추가할 수 있는 이벤트가 달려야 한다
 function addMainBlock(event) {
     var templateHtml = document.getElementById("main-block-template").innerHTML;
     // 클릭된 플러스 버튼이 속한 메인블록 아래에 메인블록이 추가된다
@@ -23,9 +24,14 @@ function addMainBlock(event) {
     createdAddMainBlockBtn.onclick = addMainBlock;
 }
 
-function addOnClickToAddMainBlockBtns(addMainBlockBtns) {
-    for (var i = 0; i < addMainBlockBtns.length; i++) {
-        var addMainBlockBtn = addMainBlockBtns[i];
-        addMainBlockBtn.onclick = addMainBlock;
+function removeMainBlock(event) {
+    var clickedMainBlock = event.target.closest(".main-block");
+    clickedMainBlock.remove();
+}
+
+function addOnClickToMainBlockBtns(mainBlockBtns, onClickMethod) {
+    for (var index = 0; index < mainBlockBtns.length; index++) {
+        var mainBlockBtn = mainBlockBtns[index]
+        mainBlockBtn.onclick = onClickMethod;
     }
 }
